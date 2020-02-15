9db36f76939c949e2ebbf64c502fec1711ee705d
mertbubax <59665720+mertbubax@users.noreply.github.com>
2020-02-15 14:52:10 +0300

    Add files via upload

---
+++ checkhost.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+# Check-Host Savar Python v1
+# #Skype : Polatbay54@gmail.com
+import os
+os.system('iptables -A INPUT -s 88.119.179.10/24 -j DROP')
+os.system('iptables -A INPUT -s 5.149.253.121/24 -j DROP')
+os.system('iptables -A INPUT -s 149.202.25.65/24 -j DROP')
+os.system('iptables -A INPUT -s 213.202.222.130/24 -j DROP')
+os.system('iptables -A INPUT -s 46.4.143.48/24 -j DROP')
+os.system('iptables -A INPUT -s 185.25.204.60/24 -j DROP')
+os.system('iptables -A INPUT -s 193.187.152.50/24 -j DROP')
+os.system('iptables -A INPUT -s 195.123.211.84/24 -j DROP')
+os.system('iptables -A INPUT -s 178.17.171.235/24 -j DROP')
+os.system('iptables -A INPUT -s 31.148.219.169/24 -j DROP')
+os.system('iptables -A INPUT -s 51.68.27.192/24 -j DROP')
+os.system('iptables -A INPUT -s 212.129.21.56/24 -j DROP')
+os.system('iptables -A INPUT -s 176.111.58.244/24 -j DROP')
+os.system('iptables -A INPUT -s 176.221.34.58/24 -j DROP')
+os.system('iptables -A INPUT -s 95.216.93.88/24 -j DROP')
+os.system('iptables -A INPUT -s 185.159.82.88/24 -j DROP')
+os.system('iptables -A INPUT -s 192.157.233.160/24 -j DROP')
+os.system('iptables -A INPUT -s 109.248.203.193/24 -j DROP')
+os.system('iptables -A INPUT -s 45.80.70.224/24 -j DROP')
+os.system('iptables -A INPUT -s 198.56.183.15/24 -j DROP')
+os.system('iptables -A INPUT -s 172.246.126.50/24 -j DROP')
+os.system('iptables -A INPUT -s 185.86.77.126/24 -j DROP')
+os.system('iptables -A INPUT -s 185.209.161.169/24 -j DROP')
+os.system('iptables -A INPUT -s 185.29.8.135/24 -j DROP')
+os.system('iptables -A INPUT -s 139.99.75.183/24 -j DROP')
+os.system('iptables -A INPUT -s 93.170.128.167/24 -j DROP')
+os.system('iptables -A INPUT -s 185.120.77.165/24 -j DROP')
+os.system('iptables -A INPUT -s 179.43.148.195/24 -j DROP')
+os.system('iptables -A INPUT -s 5.22.157.52/24 -j DROP')
+os.system('iptables -A INPUT -s 95.46.98.26/24 -j DROP')
+os.system('iptables -A INPUT -s 185.81.113.195/24 -j DROP')
+os.system('iptables -A INPUT -s 199.38.245.218/24 -j DROP')
+os.system('iptables -A INPUT -s 146.196.65.20/24 -j DROP')
+os.system('iptables -A INPUT -s 31.148.219.0/24 -j DROP')
+print """
+\033[37m 
+                                  ██▌
+                               ▄▄███▀
+                              █████ ▄█░
+                           ▄████████▀
+                          ▄█████████
+                           ▄███████▌
+                         ▄█████████
+                      ▄███████████▌
+                ▄▄▄▄██████████████▌
+            ▄▄███████████████████▌
+          ▄██████████████████████
+         ████████████████████████
+ █     ▐██████████▌ ▀▀███████████
+▐██   ▄██████████▌         ▀██▐█▌
+ ██████ █████████           ▐█▐█▌
+  ▀▀▀▀   ██████▀            ▐█▐█▌
+         ▐█████▌            ▐█▐█▌
+          ███▀██             █ █
+         ▐██   ██        ▄▄████████▄
+         ██▌    █▄     ▄███████████████████
+         ▐██    ██▄▄███████████████████████
+          ▐██  ▄████████████████████████████
+          ▄▄████████████████████████████████
+████████████████████████████████████████████
+
+
+"""                       
+print "-----------=[ Check \033[91mHost/\033[94m Ip Engelleme Başarılı ]=-----------"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+print "          ....................................\033[94m "
+print "          .  [+] \033[91mYapımcı :\033[94mxr4zz3rs       ."
+print "          . [+] \033[91mWebsite :\033[94m RealityCheats.COM!  ."
+print "          .[+] \033[91mGithub :\033[94mgithub.com/xr4zz3rs . "
+print "          ....................................  " 
+
+ 
+
+
